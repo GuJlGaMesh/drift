426272df0ea0afaf38049100efea0c21b96e0106
Vladimir <sirix.vladim@gmail.com>
2021-11-19 11:20:03 +0300

    commit

---
+++ drift/Data/Migrations/20211118143456_polya.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace drift.Data.Migrations
+{
+    public partial class polya : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.AddColumn<int>(
+		        name: "Total",
+		        table: "CompetitionScores",
+		        type: "int",
+		        nullable: false,
+		        defaultValue: 0);
+	        migrationBuilder.AddColumn<int>(
+		        name: "ParticipantNumber",
+		        table: "CompetitionResults",
+		        type: "int",
+		        nullable: false,
+		        defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
